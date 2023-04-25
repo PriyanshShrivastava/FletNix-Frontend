7ee62859fc412088f43011d5cfb0c015760d2fd9
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-04-25 14:59:18 +0530

    docs: :memo: added Homepage
    Added a protected homepage component

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+})
+export class HomepageComponent {
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>(`${environment.apiBaseUrl}movies/shows/}`)
+      .subscribe((response: any) => {
+        this.data = response.shows;
+      });
+  }
+}
