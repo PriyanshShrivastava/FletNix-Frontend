93560c6290065ba6446b7f96e529e6d1414ae2ce
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-04-26 08:56:34 +0530

    refactor: :sparkles: added form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment.development';
 
 @Component({
@@ -8,26 +15,61 @@ import { environment } from 'src/environments/environment.development';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
-  constructor(private http: HttpClient, private router: Router) {}
+export class RegisterComponent implements OnInit {
+  myForm!: FormGroup;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService,
+    private fb: FormBuilder
+  ) {}
 
-  // Object to get data from the input
-  public data = {
-    name: '',
-    email: '',
-    password: '',
-    age: '',
-  };
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      age: ['', Validators.required],
+    });
+  }
 
   // sign up handler function
-  onSignUp = () => {
-    console.log(this.data);
-    this.http
-      .post(`${environment.apiBaseUrl}auth/register`, this.data)
-      .subscribe((data: any) => {
-        if (data.success) {
-          this.router.navigate(['/']);
+  submitForm = (event: Event): void => {
+    console.log('function called');
+    event.preventDefault();
+    if (this.myForm.valid) {
+      const data = {
+        name: this.myForm.controls['name'].value,
+        email: this.myForm.controls['email'].value,
+        password: this.myForm.controls['password'].value,
+        age: this.myForm.controls['age'].value,
+      };
+
+      this.http.post(`${environment.apiBaseUrl}auth/register`, data).subscribe(
+        (data: any) => {
+          if (data.success) {
+            this.toastr.success('Account Created Succesfully ');
+            setTimeout(() => {
+              this.router.navigate(['/']);
+            }, 1500);
+          } else {
+            this.toastr.error(
+              'Error creating account. Please try again later.'
+            );
+          }
+        },
+        (error) => {
+          // If there's an error with the API call, show an error message
+          this.toastr.error('Error creating account. Please try again later.');
         }
-      });
+      );
+    } else {
+      // If the form is not valid, show an error message
+      this.toastr.error('Please fill the form correctly.');
+
+      setTimeout(() => {
+        this.myForm.reset();
+      }, 1500);
+    }
   };
 }
